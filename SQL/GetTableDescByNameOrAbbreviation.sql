d37e70b7ee0da35655f4c393438822b5255912e8
Bob Delamater <bob.delamater@sage.com>
2016-01-20 07:28:01 -0800

    Update GetTableDescByNameOrAbbreviation.sql

--- SQL/GetTableDescByNameOrAbbreviation.sql
+++ SQL/GetTableDescByNameOrAbbreviation.sql
@@ -1,8 +1,8 @@
 -- Press Ctrl+Shift+M to replace template parameters
 -- Get Table Description by Abbreviation Or Name
 SELECT t.ABRFIC_0 TableAbreviation, t.CODFIC_0 TableName, txt.TEXTE_0
-FROM <X3 Folder Name,VARCHAR(255), AATCPROD >.ATABLE t
-	INNER JOIN <X3 Folder Name,VARCHAR(255), AATCPROD >.ATEXTE txt
+FROM <X3 Folder Name,VARCHAR(255), SEED >.ATABLE t
+	INNER JOIN <X3 Folder Name,VARCHAR(255), SEED >.ATEXTE txt
 		ON t.INTITFIC_0 = txt.NUMERO_0
 		AND txt.LAN_0 = 'ENG'
 WHERE 
