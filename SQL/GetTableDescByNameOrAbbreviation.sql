8fbd21e9657bf2dccd6bb58c11ee5b73947a545a
Bob Delamater <bob.delamater@sage.com>
2016-01-20 07:27:08 -0800

    Create GetTableDescByNameOrAbbreviation.sql

---
+++ SQL/GetTableDescByNameOrAbbreviation.sql
@@ -0,0 +1,9 @@
+-- Get Table Description by Abbreviation Or Name
+SELECT t.ABRFIC_0 TableAbreviation, t.CODFIC_0 TableName, txt.TEXTE_0
+FROM <X3 Folder Name,VARCHAR(255), AATCPROD >.ATABLE t
+	INNER JOIN <X3 Folder Name,VARCHAR(255), AATCPROD >.ATEXTE txt
+		ON t.INTITFIC_0 = txt.NUMERO_0
+		AND txt.LAN_0 = 'ENG'
+WHERE 
+	CODFIC_0 = 'CACCE'		-- If you know the table name
+	OR ABRFIC_0 = 'CCE'		-- If you know the table abbreviation
