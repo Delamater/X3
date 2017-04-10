1520e1772019f49fec9f5acd13bcae1b949dee36
Bob Delamater <bob.delamater@sage.com>
2017-04-10 15:03:28 -0700

    Count the number of iterations
    Adding the count of the number of iterations for this automation test

--- Powershell/Automation/CopyFolder_Overwrite.ps1
+++ Powershell/Automation/CopyFolder_Overwrite.ps1
@@ -2,8 +2,27 @@
 #Date: 02/08/2017
 #Description: Copy folder down from one location to another, overwrite. 
 
-$SourceFolder = "Q:\X3V11_P-FLOW-GESPIH-TWM-05"
-$TargetFolder = "C:\WorkingFolder\X3\Automation\Scripts"
+$SourceFolder = Read-Host -Prompt "Path to test:" 
+#$SourceFolder = "\X3VNEXT_M-FLOW-GESMFG-PS-3"
+$FullPathSourceFolder = "Q:\" + $SourceFolder
+$TargetFolder = "C:\Users\bdelamater\Google Drive\Sage\X3\WorkingFolder\Automation\Scripts"
 
-$CopyResult = Copy-Item -Path $SourceFolder -Destination $TargetFolder -Recurse -Container -Force
-if(-not $?) {write-warning "Copy Failed"}
+#$CopyResult = Copy-Item -Path $FullPathSourceFolder -Destination $TargetFolder -Recurse -Container -Force
+#if(-not $?) {write-warning "Copy Failed"}
+
+
+# Check local file list, look for the number of iterations
+$files = Get-ChildItem $TargetFolder\$SourceFolder -Filter *IT*.htm
+
+$iterationValues = New-Object System.Collections.Generic.List[System.Object]
+
+foreach ($file in $files){
+    $startPos =$file.ToString().IndexOf("_IT")
+    $iterationValues.Add( $file.ToString().SubString($startPos+3,1) )
+}
+
+$iterationValues|measure -Maximum
+
+
+#Cleanup
+$iterationValues.Clear()
