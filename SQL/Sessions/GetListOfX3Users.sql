45a479a54a7078312596ad68beeff0088d7ab97a
Bob Delamater <bob.delamater@sage.com>
2019-07-26 13:26:04 -0700

    Adding hostprocess to output
    Allows the caller to plug the sp.hostprocess value directly into profiler and get all spids that execute on a single sadoss connection. More than one spid can execute.

--- SQL/Sessions/GetListOfX3Users.sql
+++ SQL/Sessions/GetListOfX3Users.sql
@@ -1,9 +1,31 @@
-Select X3SM1.SESSIONID_0 as 'Session_ID', X3SM0.SESSIONTYPE_0,  X3SM1.DBIDENT1_0 as 'SPID', X3SM1.DBIDENT2_0, X3SM1.CREUSR_0, X3SM1.UPDTICK_0, X3SM0.ALOGIN_0 as 'Login Syracuse', X3SM0.FOLD_0 as 'DOSSIER', X3SM0.LAN_0 as 'Langue', X3SM0.PEER_0 as 'IP Client', X3SM0.PROCESSADX_0 as 'Port moteur',
-X3AFU.FCT_0 as 'Fonction', X3AFU.USR_0 as 'User X3',  X3SM2.PROCESSNAME_0, X3SM2.SYSTEMID_0 as 'PID OS', X3SM2.PORT_0 as 'Port client'
-from X3.ASYSSMDBASSO X3SM1 Join X3.ASYSSMINTERN X3SM0 
-              on X3SM1.SESSIONID_0 = X3SM0.SESSIONID_0 
-              Join X3.AFCTCUR X3AFU on X3AFU.UID_0=X3SM1.SESSIONID_0
-              Join X3.ASYSSMPROCES X3SM2 on X3SM2.SESSIONID_0=X3SM1.SESSIONID_0
-Join  sys.dm_exec_connections VDS On VDS.session_id = convert(integer, X3SM1.DBIDENT1_0) 
- and VDS.connect_time = convert(datetime, X3SM1.DBIDENT2_0, 121 ) 
-where X3SM0.FOLD_0='MYFOLDER' --and X3SM0.ALOGIN_0='admin'
+Select 
+	X3SM1.SESSIONID_0 as 'Session_ID', 
+	X3SM0.SESSIONTYPE_0,  
+	X3SM1.DBIDENT1_0 as 'SPID', 
+	sp.hostprocess as 'Profiler ClientProcessID',
+	X3SM1.DBIDENT2_0, 
+	X3SM1.CREUSR_0, 
+	X3SM1.UPDTICK_0, 
+	X3SM0.ALOGIN_0 as 'Login Syracuse', 
+	X3SM0.FOLD_0 as 'DOSSIER', 
+	X3SM0.LAN_0 as 'Langue', 
+	X3SM0.PEER_0 as 'IP Client', 
+	X3SM0.PROCESSADX_0 as 'Port moteur',
+	X3AFU.FCT_0 as 'Fonction', 
+	X3AFU.USR_0 as 'User X3',  
+	X3SM2.PROCESSNAME_0, 
+	X3SM2.SYSTEMID_0 as 'PID OS', 
+	X3SM2.PORT_0 as 'Port client'
+from X3.ASYSSMDBASSO X3SM1 
+	JOIN X3.ASYSSMINTERN X3SM0 
+		ON X3SM1.SESSIONID_0 = X3SM0.SESSIONID_0 
+	JOIN X3.AFCTCUR X3AFU 
+		ON X3AFU.UID_0=X3SM1.SESSIONID_0
+	JOIN X3.ASYSSMPROCES X3SM2 
+		ON X3SM2.SESSIONID_0=X3SM1.SESSIONID_0
+	JOIN  sys.dm_exec_connections VDS 
+		ON VDS.session_id = convert(integer, X3SM1.DBIDENT1_0) 
+			and VDS.connect_time = convert(datetime, X3SM1.DBIDENT2_0, 121 ) 
+	JOIN sys.sysprocesses sp
+		ON VDS.session_id = sp.spid
+WHERE X3SM0.FOLD_0='<X3 Folder Name, SYSNAME, SEED>' --and X3SM0.ALOGIN_0='<X3 User Login, NVARCHAR(40), admin>'
