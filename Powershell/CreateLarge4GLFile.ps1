6a4c6d194ce40ca34d984e9c496c15f6078ee50e
Bob Delamater <bob.delamater@sage.com>
2020-11-10 06:27:28 -0800

    Update CreateLarge4GLFile.ps1

--- Powershell/CreateLarge4GLFile.ps1
+++ Powershell/CreateLarge4GLFile.ps1
@@ -1,15 +1,63 @@
 $fileDestination = "c:\temp\LargeFile.src"
-function file-builder($fileName, $nTimes) {
+#$envOptions = "`n" #"`r`n"
+$envOptions = environment::newline
+$nTimes = 2
+$runtimePath = "D:\Sage\X3ERPV12\Runtime"
+$cmd = "valtrt -v -l ""ENG"" -f %ADXDIR%/../Folders/X3/TRT/ZLARGE01 -d %ADXDIR%/../Folders/X3/TRT"
+$combinedPath = "$envBat;$cmd"
+
+
+function Get-Environment-Script{
+    <#
+        .SYNOPSIS
+            Return the path to the env.bat file. Test to ensure that the path is valid.
+        .INPUTS
+            runtimePath: The full path of Runtime (ADXDIR), such as C:\Sage\EMV12\Runtime.
+        .OUTPUTS
+            envBatPath: The full path of Runtime env.bat file (should be ADXDIR\bin)
+    #>
+    param(
+        [Parameter(Mandatory = $true, Position = 1)] [string] $runtimePath
+    )
+
+    $envBatPath = Join-Path -Path "$runtimePath" -ChildPath "bin" | Join-Path -ChildPath "env.bat"
+    #Test-FileOrDir-EXIST -DIRorFILE $envBatPath
+
+    return $envBatPath
+}
+
+# Execute a system command
+# Return a structure which includes stderr and stdout 
+function Invoke-Command ($commandTitle, $commandPath, $commandArguments) {
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = $commandPath
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $commandArguments
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $p.WaitForExit()
+    [pscustomobject]@{
+        commandTitle = $commandTitle
+        stdout       = $p.StandardOutput.ReadToEnd()
+        stderr       = $p.StandardError.ReadToEnd()
+        ExitCode     = $p.ExitCode
+    }
+}
+
+function file-builder($nTimes) {
 $result = ""
     for ($i=0; $i -lt $nTimes; $i++){
-        $result +="Funprog TESTCASE_$i`r`n"
+        $result +="Funprog TESTCASE_$i $envOptions"
         $result +="`r`n"
-        $result +="   Local Char MESSAGE(250)`r`n"
-        $result += "   MESSAGE=""This is a message""`r`n"
-        $result += "   RET=0`r`n"
-        $result += "`r`n"
-        $result += "End RET`r`n"
-        $result += "`r`n"
+        $result +="   Local Char MESSAGE(250) $envOptions"
+        $result += "   MESSAGE=""This is a message"" $envOptions"
+        $result += "   RET=$i $envOptions"
+        $result += $envOptions
+        $result += "End RET $envOptions"
+        $result += $envOptions
 
     }
 
@@ -19,7 +67,13 @@ $result = ""
 
 
 
-$retVal = ""
-$retVal = file-builder -fileName "Some anem" -nTimes 5
-Write-Host "result: $retVal" 
-Out-File -FilePath $fileDestination -InputObject $retVal
+
+
+$retVal = file-builder -nTimes $nTimes 
+Out-File -FilePath $fileDestination -InputObject $retVal -Encoding utf8
+#Copy-Item -Path $fileDestination -Destination "$runtimePath/../folders/X3/TRT/ZLARGE05.src"
+
+#$envBat = Get-Environment-Script -runtimePath $runtimePath
+#Invoke-Command -commandTitle "VALTRT" -commandPath "cmd" -commandArguments "/c " + $envBat + " " + $cmd 
+
+#valtrt -v -l "ENG" -f %ADXDIR%/../Folders/X3/TRT/LargeFile -d %ADXDIR%/../Folders/X3/TRT
